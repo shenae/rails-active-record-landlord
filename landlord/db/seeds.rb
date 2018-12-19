c140d0df3256e61d9bf726e1079f2237c151f32d
John Master <john.master@ga.co>
2018-12-19 12:13:03 -0500

    Add seeds

--- landlord/db/seeds.rb
+++ landlord/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Apartment.create(
+  unit: 'A',
+  num_beds: 4,
+  num_baths: 2,
+  monthly_rent: 700,
+  allows_pets: true,
+  square_feet: 2000
+)
+
+Tenant.create(
+  first_name: 'Jane',
+  last_name: 'Doe',
+  birth_date: Date.new(1975, 6, 5),
+  nickname: 'J',
+  occupation: 'Software Developer',
+  ssn: '123-46-7733',
+  has_pets: true
+)
